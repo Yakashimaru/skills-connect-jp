c4d3d9f0cb8aad9830f4549d6b05ee3298629787
A <a@As-MacBook-Air.local>
2026-06-11 00:11:29 +1000

    feat: add pre-rendering with react-snap for Google indexing

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,24 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot, hydrateRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import './i18n'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext'
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root')!
+const app = (
   <StrictMode>
     <HelmetProvider>
       <AuthProvider>
         <App />
       </AuthProvider>
     </HelmetProvider>
-  </StrictMode>,
+  </StrictMode>
 )
+
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, app)
+} else {
+  createRoot(rootElement).render(app)
+}
